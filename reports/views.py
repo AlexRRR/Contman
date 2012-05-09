e70818834d54bac8aea991990e6d58627205a7c2
Alejandro Ramirez <alexrrr@hotmail.com>
2012-05-09 10:17:01 +0200

    Added report data generators
    added several functions to add report capabilites (by month, by date)

--- reports/views.py
+++ reports/views.py
@@ -47,9 +47,9 @@ def to_georgian(datestring):
 
 def to_month_number(datestring):
     '''converts a date string into a month number'''
-    pdb.set_trace()
+    if ' ' in datestring:
+        datestring = datestring.split()[0]
     month = datetime.strptime(datestring,'%Y-%m-%d').date().month
-    pdb.set_trace()
     return month
 
 def extract_week_no(ds):
@@ -57,9 +57,9 @@ def extract_week_no(ds):
         return d
 
 def next_month(last_result):
-    last_date = date.strptime(last_result,'%Y-%m-%d').date()
+    last_date = datetime.strptime(last_result,'%Y-%m-%d').date()
     m = last_date.month
-    y = last_result.year
+    y = last_date.year
     m += 1
     if m == 13:
         m = 1
@@ -67,22 +67,29 @@ def next_month(last_result):
     first_of_next_month = date(y, m, 1)
     return first_of_next_month.strftime('%Y-%m-%d')
 
-def next_day(last_result):
-    return date.fromordinal(to_georgian(last_result()) + 1).strftime('%Y-%m-%d')
+def next_day(current_day):
+    return date.fromordinal(to_georgian(current_day) + 1).strftime('%Y-%m-%d')
 
-def fill_gaps(results,qtype,end_d):
-    '''adds entries to list for periods where not data was found'''
+def fill_gaps(results,qtype,start_d,end_d):
+    '''adds entries to list with 0es for dates where not data was found'''
     dispatch={'by_day':to_georgian,'by_month':to_month_number}
     next_date={'by_day':next_day,'by_month':next_month} 
     stored_results = list(results)
     end_date = datetime.strptime(end_d,'%Y-%m-%d').date()
+    start_date = datetime.strptime(start_d,'%Y-%m-%d').date()
 
+    #add start date and end date to list if no results, to make sure it is filled with 0s
     end_date_found = False
+    start_date_found = False
     for result in stored_results:
         if end_date in result:
             end_date_found = True
+        if start_date in result:
+            start_date_found = True
     if not end_date_found:
         stored_results.append({'date_created': end_d, 'created_count': 0})
+    if not start_date_found:
+        stored_results.insert(0,{'date_created': start_d, 'created_count': 0})
 
     no_gap_results = []
     last_result = lambda:no_gap_results[-1]['date_created']
@@ -95,13 +102,12 @@ def fill_gaps(results,qtype,end_d):
                 no_gap_results.append(entry)
             else:
                 while (dispatch[qtype](entry['date_created']) - dispatch[qtype](last_result()) != 1):
-                    next_date = next_date[qtype]
-                    next_empty_entry = {'date_created': next_date, 'created_count': 0}
+                    next_d = next_date[qtype](last_result())
+                    next_empty_entry = {'date_created': next_d, 'created_count': 0}
                     no_gap_results.append(next_empty_entry)
                 no_gap_results.append(entry)
                 continue
     return no_gap_results
-
 
 
 def report_by_date(start_d,end_d):
@@ -110,37 +116,29 @@ def report_by_date(start_d,end_d):
     start_date = datetime.strptime(start_d,'%Y-%m-%d').date()
     end_date = datetime.strptime(end_d,'%Y-%m-%d').date()
     query_results = SMS.objects.filter(received__gt=start_date).exclude(received__gt=end_date).extra({'date_created' : "date(received)"}).values('date_created').annotate(created_count=Count('id'))
-    stored_results = list(query_results)
+    return fill_gaps(query_results,'by_day',start_d, end_d)
 
-    end_date_found = False
-    for result in stored_results:
-        if end_date in result:
-            end_date_found = True
-    if not end_date_found:
-        stored_results.append({'date_created': end_d, 'created_count': 0})
 
-    no_gap_results = []
-    last_result = lambda:no_gap_results[-1]['date_created']
-    for entry in stored_results:
-        if len(no_gap_results) == 0:
-            no_gap_results.append(entry)
-            continue
+def remove_time(datelist):
+    '''Removes time information for each datetime string in list "datelist" returning
+    a new list with only dates'''
+    clean_results = []
+    for entry in datelist:
+        if ' ' in entry['date_created']:
+            only_date = entry['date_created'].split()[0]
+            clean_results.append({'date_created':only_date,'created_count':entry['created_count']})
         else:
-            if to_georgian(entry['date_created']) - to_georgian(last_result()) == 1:
-                no_gap_results.append(entry)
-            else:
-                while (to_georgian(entry['date_created']) - to_georgian(last_result()) != 1):
-                    next_date = date.fromordinal(to_georgian(last_result()) + 1).strftime('%Y-%m-%d')
-                    next_empty_entry = {'date_created': next_date, 'created_count': 0}
-                    no_gap_results.append(next_empty_entry)
-                no_gap_results.append(entry)
-                continue
-    return no_gap_results
+            clean_results.append(entry)
+    return clean_results
 
 
+def report_by_month(start_d,end_d):
+    start_date = datetime.strptime(start_d,'%Y-%m-%d').date()
+    end_date = datetime.strptime(end_d,'%Y-%m-%d').date()
 
-def report_by_month():
-    a = SMS.objects.extra(select={'date_created': connections[SMS.objects.db].ops.date_trunc_sql('month', 'received')}).values('date_created').annotate(created_count=Count('received'))
-    return fill_gaps(list(a),'by_month','2012-05-10')
+    query_results = SMS.objects.filter(received__gt=start_date).exclude(received__gt=end_date).extra(select={'date_created': connections[SMS.objects.db].ops.date_trunc_sql('month', 'received')}).values('date_created').annotate(created_count=Count('received'))
+    results = remove_time(query_results)
+
+    return fill_gaps(results,'by_month',start_d, end_d)
 
     
